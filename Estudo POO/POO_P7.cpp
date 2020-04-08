21f842e907dbff0e6c1012bc37c3d3cd699c2290
miguel <guegosilvestre67@gmail.com>
2020-04-08 16:37:16 -0300

    Arrumando algumas cois o P6 e criando o P7

--- Estudo POO/POO_P7.cpp
+++ Estudo POO/POO_P7.cpp
@@ -4,20 +4,28 @@ using namespace std;
 
 class Carro{
 private:
-public:
     int velMax;
-    const int val_imag1 = 100,val_imag2 = 200;
+public:
+    const int vel_imag1 = 100,vel_imag2 = 200;
     int potencia;
     const char* nome;
+    void setVelMax(int velocidade_maxima);
+    int getVelMax(){
+        return this->velMax;
+    }
     Carro(const char* n, int p);
     
 };
 
+void Carro::setVelMax(int velocidade_maxima){
+    this->velMax = velocidade_maxima;
+}
+
 Carro::Carro(const char* n, int p):nome(n),potencia(p){// lista de inicialização
-        if(p < val_imag1)
+        if(p < vel_imag1)
         {
             this->velMax = 120;
-        }else if (p < val_imag2)
+        }else if (p < vel_imag2)
         {
             this->velMax = 220;
         }else
@@ -28,8 +36,10 @@ Carro::Carro(const char* n, int p):nome(n),potencia(p){// lista de inicializaç�
         
     }
 int main(){
-    unique_ptr<Carro>c1(new Carro{"Bruno",70});
+    unique_ptr<Carro>c1(new Carro{"Bruno",85});
     cout << c1->nome << " - " << c1->potencia << " - " << c1->velMax << endl;
-    
+    Carro c2{"Ventania",400};
+    cout << c2.nome << " - " << c2.potencia << " - " << c2.velMax << endl;
+
     return 0;
 }
