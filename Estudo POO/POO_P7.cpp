a484f4f13a2c26916cf48a822fac3c5169cccd1a
miguel <guegosilvestre67@gmail.com>
2020-04-08 17:40:31 -0300

    Commit_P9

--- Estudo POO/POO_P7.cpp
+++ Estudo POO/POO_P7.cpp
@@ -5,41 +5,39 @@ using namespace std;
 class Carro{
 private:
     int velMax;
+    void setVelMax(int velocidade_maxima){this->velMax = velocidade_maxima;}
+
 public:
     const int vel_imag1 = 100,vel_imag2 = 200;
     int potencia;
     const char* nome;
-    void setVelMax(int velocidade_maxima);
-    int getVelMax(){
-        return this->velMax;
-    }
+    int getVelMax(){return this->velMax;}
     Carro(const char* n, int p);
     
 };
 
-void Carro::setVelMax(int velocidade_maxima){
-    this->velMax = velocidade_maxima;
+Carro::Carro(const char* n, int p):nome(n),potencia(p){// lista de inicialização
+    // operador ternario ()? <> : <>; 
+    if(p < vel_imag1)
+    {
+        //this->setVelMax(120)
+        this->velMax = 120;
+    }else if (p < vel_imag2)
+    {
+        //this->setVelMax(220)
+        this->velMax = 220;
+    }else
+    {
+        //this->setVelMax(350)
+        this->velMax = 350;
+    }     
 }
 
-Carro::Carro(const char* n, int p):nome(n),potencia(p){// lista de inicialização
-        if(p < vel_imag1)
-        {
-            this->velMax = 120;
-        }else if (p < vel_imag2)
-        {
-            this->velMax = 220;
-        }else
-        {
-            this->velMax = 350;
-        }
-        
-        
-    }
 int main(){
     unique_ptr<Carro>c1(new Carro{"Bruno",85});
-    cout << c1->nome << " - " << c1->potencia << " - " << c1->velMax << endl;
+    cout << c1->nome << " - " << c1->potencia << " - " << c1->getVelMax() << endl;
     Carro c2{"Ventania",400};
-    cout << c2.nome << " - " << c2.potencia << " - " << c2.velMax << endl;
+    cout << c2.nome << " - " << c2.potencia << " - " << c2.getVelMax() << endl;
 
     return 0;
 }
