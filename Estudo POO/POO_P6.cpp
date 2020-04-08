9f59db202f92cd7c1a90eb72aed8d9a3c089edf0
miguel <guegosilvestre67@gmail.com>
2020-04-08 14:49:51 -0300

    Commit_P8 em orientação a obijeto, alterado o arquivo POO_P6.cpp

--- Estudo POO/POO_P6.cpp
+++ Estudo POO/POO_P6.cpp
@@ -9,10 +9,11 @@ public:
     const int val_imag1 = 100,val_imag2 = 200;
     int potencia;
     const char* nome;
+    Carro(const char* n, int p);
+    
+};
 
-    Carro(const char* n, int p){
-        this->nome = n;
-        this->potencia = p;
+Carro::Carro(const char* n, int p):nome(n),potencia(p){// lista de inicialização
         if(p < val_imag1)
         {
             this->velMax = 120;
@@ -26,13 +27,9 @@ public:
         
         
     }
-};
-
 int main(){
-    Carro *c1 = new Carro("Bruno",300);
-    cout << c1->nome << " - " << c1->potencia << " - " << c1->velMax << endl;
-
-    delete c1;
+    Carro c1{"Bruno",70};
+    cout << c1.nome << " - " << c1.potencia << " - " << c1.velMax << endl;
 
     return 0;
 }
