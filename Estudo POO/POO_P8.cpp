cc5ad6a53ea60098e1feb53378b72d5b68b4e196
miguel <guegosilvestre67@gmail.com>
2020-04-12 18:34:27 -0300

    POO_P8.cpp criado

---
+++ Estudo POO/POO_P8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Carro
+{
+private:
+    /* data */
+public:
+    Carro(/* args */);
+    ~Carro();
+};
+
+Carro::Carro(/* args */)
+{
+}
+
+Carro::~Carro()
+{
+}
+
+int main(){
+
+    return 0;
+}
