5373977d0fda3c9c500e1ef26ab6872854f8b8fc
miguel <guegosilvestre67@gmail.com>
2020-04-14 13:13:34 -0300

    termino de POO e inicio de Ponteiro

--- Estudo POO/POO_P8.cpp
+++ Estudo POO/POO_P8.cpp
@@ -5,21 +5,41 @@ using namespace std;
 class Carro
 {
 private:
-    /* data */
+	int velMax;
+	const char* nome;	
 public:
-    Carro(/* args */);
-    ~Carro();
+	int potencia;
+	int getVelMax(){
+		return velMax;
+	}
+	const char *getNome(){
+		return nome;
+	}
+	Carro(){
+		velMax = 120;
+		potencia = 85;
+		nome = "Popular";
+	}
+	Carro(int pt,const char *no):potencia(pt),nome(no){
+		if(pt<100){
+			velMax = 120;
+		}else if (pt<200)
+		{
+			velMax = 240;
+		}else
+		{
+			velMax = 360;
+		}
+	}
 };
 
-Carro::Carro(/* args */)
-{
-}
-
-Carro::~Carro()
-{
-}
 
 int main(){
-
+	Carro c1;
+	cout << c1.getNome() << " - " << c1.potencia << " - "<< c1.getVelMax() <<endl;
+	Carro c2{180,"Luxo"};
+	cout << c2.getNome() << " - " << c2.potencia << " - "<< c2.getVelMax() <<endl;
+	Carro c3{300,"Esportivo"};
+	cout << c3.getNome() << " - " << c3.potencia << " - "<< c3.getVelMax() <<endl;
     return 0;
 }
---
+++ Estudo POO/POO_P9.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+class Veiculo{
+private:
+	const char* nome;
+	const char* cor;
+public:
+	int velMax;
+	int rodas;
+	void setNome(const char *no){
+		nome = no;
+	}
+	const char* getNome(){
+		return nome;
+	}
+	void setCor(const char* co){
+		cor =co;
+	}
+	const char* getCor(){
+		return cor;
+	}
+	virtual void imp(){
+		cout << "Nome............:" << nome << endl;
+		cout << "Cor.............:" << cor << endl;
+		cout << "Rodas...........:" << rodas << endl;
+		cout << "Vel.Max.........:" << velMax << endl << endl;
+	}
+};
+
+class Carro:public Veiculo{
+public:
+	Carro(){
+		velMax = 160;
+		rodas = 4;
+		setNome("Carro");
+		setCor("Branco");
+	}
+};
+
+class Moto:public Veiculo{
+public:
+	Moto(){
+		velMax = 200;
+		rodas = 2;
+		setNome("Moto");
+		setCor("Preto");
+	}
+};
+
+class Militar:public Veiculo{
+public:
+	int monicao;
+	bool armamento;
+	Militar(bool arma, int mo):armamento(arma),monicao(mo){
+		velMax = 150;
+		rodas = 6;
+		setNome("Tanque");
+		setCor("Verde");
+		(arma)? monicao = mo : monicao = 0;
+	}
+	void imp() override{
+                cout << "Nome............:" << getNome() << endl;
+                cout << "Cor.............:" << getCor() << endl;
+                cout << "Rodas...........:" << rodas << endl;
+		cout << "Vel.Max.........:" << velMax << endl;
+		cout << "Armamento.......:" << armamento << endl;
+		cout << "Municao.........:" << monicao << endl << endl;
+        }
+
+};
+
+int main(){
+	Carro v1;
+	v1.imp();
+	Moto v2;
+	v2.imp();
+	Militar v3{false,200};
+	v3.imp();
+	Militar v4{true,100};
+	v4.imp();
+	return 0;
+}
